50c0ca92e1a01b22cf80e257cc287ae7618e99bf
liriling <li-rl@health-100.cn>
2024-03-06 16:04:24 +0800

    124

--- yyhome-leetcode/src/main/java/com/yyhome/leetcode/hard/MaxPathSum124.java
+++ yyhome-leetcode/src/main/java/com/yyhome/leetcode/hard/MaxPathSum124.java
@@ -20,6 +20,11 @@ public class MaxPathSum124 {
         System.out.println(new MaxPathSum124().maxPathSum(TreeNode.createTree(-3)));
         System.out.println(new MaxPathSum124().maxPathSum(TreeNode.createTree(1, 2, 3)));
         System.out.println(new MaxPathSum124().maxPathSum(TreeNode.createTree(-10,9,20,null,null,15,7)));
+
+        System.out.println(new MaxPathSum124().maxPathSum2(TreeNode.createTree(1,-2,-3,1,3,-2,null,-1)));
+        System.out.println(new MaxPathSum124().maxPathSum2(TreeNode.createTree(-3)));
+        System.out.println(new MaxPathSum124().maxPathSum2(TreeNode.createTree(1, 2, 3)));
+        System.out.println(new MaxPathSum124().maxPathSum2(TreeNode.createTree(-10,9,20,null,null,15,7)));
     }
 
     private int max = Integer.MIN_VALUE;
@@ -52,4 +57,29 @@ public class MaxPathSum124 {
         return Math.max(root.val, Math.max(leftPlus, rightPlus));
     }
 
+    /**
+     * 执行耗时:1 ms,击败了37.52% 的Java用户
+     * 内存消耗:43.4 MB,击败了23.26% 的Java用户
+     */
+    int res = Integer.MIN_VALUE;
+    public int maxPathSum2(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            res = Math.max(res, node.val);
+            return node.val;
+        }
+        int left = 0, right = 0;
+        if (node.left != null) {
+            left = dfs(node.left);
+        }
+        if (node.right != null) {
+            right = dfs(node.right);
+        }
+        res = Math.max(res, Math.max(left, 0) + Math.max(right, 0) + node.val);
+        return Math.max(left, Math.max(right, 0)) + node.val;
+    }
 }
